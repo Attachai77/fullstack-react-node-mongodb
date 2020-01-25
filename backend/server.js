879d497f0cab39de7d559c121a363429de6bf064
attachai27 <attachaisaorangtoi@gmail.com>
2020-01-25 15:57:10 +0700

    connect mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,35 @@
 const express = require('express');
+const mongoose = require('mongoose')
+const Product = require('./models/product')
+
 const app = express();
 const PORT = "3000";
+app.use(express.json())
+
+mongoose.connect('mongodb://localhost:27017/myDB', {useNewUrlParser: true})
+
+app.get('/',(req, res)=>{
+     res.send('Hello My App')
+})
+
+app.post('/products', async (req, res) => {
+     const payload = req.body
+     const product = new Product(payload)
+     await product.save()
+     res.status(201)
+     res.json({
+          product: product,
+          success: true
+     })
+})
+
+app.get('/products', async (req, res)=>{
+     const products = await Product.find()
+     res.json({
+          products: products,
+          success: true
+     })
+} )
 
 app.listen(3000, () => {
      console.log("server status : running");
